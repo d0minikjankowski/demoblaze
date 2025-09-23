b7311c865d967b674db3692c851c783047fa1655
Dominik Jankowski <dominik.jankowski@outlook.com>
2025-09-23 14:59:19 +0200

    added more products

--- cypress/page_objects/cartOperations.js
+++ cypress/page_objects/cartOperations.js
@@ -20,6 +20,42 @@ class CartOperations {
         cy.get('@alertStub').should('have.been.calledWith', 'Product added')
     }
 
+    addNokiaLumia1520() {
+        cy.get(productsLocators.nokiaLumia1520).click()
+        cy.get(productsLocators.addToCartBtn).click()
+        cy.window().then((win) => {
+            cy.stub(win, 'alert').as('alertStub')
+        })
+        cy.get('@alertStub').should('have.been.calledWith', 'Product added')
+    }
+
+    addNexus6() {
+        cy.get(productsLocators.nexus6).click()
+        cy.get(productsLocators.addToCartBtn).click()
+        cy.window().then((win) => {
+            cy.stub(win, 'alert').as('alertStub')
+        })
+        cy.get('@alertStub').should('have.been.calledWith', 'Product added')
+    }
+
+    addIphone632gb() {
+        cy.get(productsLocators.iphone632gb).click()
+        cy.get(productsLocators.addToCartBtn).click()
+        cy.window().then((win) => {
+            cy.stub(win, 'alert').as('alertStub')
+        })
+        cy.get('@alertStub').should('have.been.calledWith', 'Product added')
+    }
+
+    addSonyXperiaZ5() {
+        cy.get(productsLocators.sonyXperiaZ5).click()
+        cy.get(productsLocators.addToCartBtn).click()
+        cy.window().then((win) => {
+            cy.stub(win, 'alert').as('alertStub')
+        })
+        cy.get('@alertStub').should('have.been.calledWith', 'Product added')
+    }
+
 }
 
 export default new CartOperations();
