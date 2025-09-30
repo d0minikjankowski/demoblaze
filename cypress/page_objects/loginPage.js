de92ee1ed9000c5f9f8c1f2df55b676f4a430555
d0minikjankowski <dominik.jankowski@outlook.com>
2025-10-01 00:04:04 +0200

    chore-added-junit

--- cypress/page_objects/loginPage.js
+++ cypress/page_objects/loginPage.js
@@ -10,9 +10,9 @@ class LoginPage {
         cy.get(mainPageLocators.openLogInBtn).click()
         cy.fixture('users.json').then((users) => {
             cy.get(mainPageLocators.usernameInput).type(users.validUser.username)
-            cy.wait(1000)
+            cy.wait(5000)
             cy.get(mainPageLocators.passwordInput).type(users.validUser.password)
-            cy.wait(1000)
+            cy.wait(3000)
             cy.get(mainPageLocators.logInBtn).eq(2).click()
             cy.get(mainPageLocators.welcomeBtn).should('have.text', 'Welcome' + ' ' + users.validUser.username)
     })}
@@ -21,9 +21,9 @@ class LoginPage {
         cy.get(mainPageLocators.openLogInBtn).click()
         cy.fixture('users.json').then((users) => {
             cy.get(mainPageLocators.usernameInput).type(users.invalidUser.username)
-            cy.wait(1000)
+            cy.wait(3000)
             cy.get(mainPageLocators.passwordInput).type(users.invalidUser.password)
-            cy.wait(1000)
+            cy.wait(3000)
             cy.get(mainPageLocators.logInBtn).eq(2).click()})
             cy.on('window:alert', (alertMessage) => {
             expect(alertMessage).to.equal('User does not exist.')
